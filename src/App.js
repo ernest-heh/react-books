9dac7e267eb9048fc5e595499094352c5a32e2f3
Ernest Ho <ernest.heh@gmail.com>
2023-04-13 23:21:43 +0800

    adds ability to delete books

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
       {
-        id: Math.round(Math.floor() * 9999),
+        id: Math.round(Math.random() * 9999),
         title,
       },
     ];
@@ -19,7 +27,7 @@ function App() {
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
